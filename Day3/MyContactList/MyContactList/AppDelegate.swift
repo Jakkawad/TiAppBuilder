b276ba8acd620614baf88ca8228e812a8c17ef85
Jakkawad Chaiplee <sunnysunoc@gmail.com>
2017-09-02 10:56:27 +0700

    import GoogleMobileAds

--- Day3/MyContactList/MyContactList/AppDelegate.swift
+++ Day3/MyContactList/MyContactList/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+//import Admob
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //Add AdmobKey
+        GADMobileAds.configure(withApplicationID: "key")
         return true
     }
 
